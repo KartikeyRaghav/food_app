0641b1d6d36ab08afd08a9a704fe1dc0b894a415
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-04-13 14:19:15 +0530

    Changes

---
+++ lib/common/widgets/view_all_title_row.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/utils/constants/colors.dart';
+
+class ViewAllTitleRow extends StatelessWidget {
+  final String title;
+  final VoidCallback onView;
+  const ViewAllTitleRow({super.key, required this.title, required this.onView});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: FAColors.textPrimary,
+            fontSize: 20,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        TextButton(
+          onPressed: onView,
+          child: Text(
+            "View all",
+            style: TextStyle(
+              color: FAColors.primary,
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/presentation/screens/home/home.dart
+++ lib/features/presentation/screens/home/home.dart
@@ -1,95 +1,257 @@
 import 'package:flutter/material.dart';
-import 'package:food_app/common/styles/spacing_styles.dart';
-import 'package:food_app/features/presentation/controllers/homepage_controller.dart';
+import 'package:food_app/common/widgets/round_textfield.dart';
+import 'package:food_app/common/widgets/category_cell.dart';
+import 'package:food_app/common/widgets/most_popular_cell.dart';
+import 'package:food_app/common/widgets/popular_resutaurant_row.dart';
+import 'package:food_app/common/widgets/recent_item_row.dart';
+import 'package:food_app/common/widgets/view_all_title_row.dart';
 import 'package:food_app/utils/constants/colors.dart';
-import 'package:food_app/utils/constants/sizes.dart';
-import 'package:food_app/utils/constants/text_strings.dart';
-import 'package:food_app/utils/theme/widget_themes/search_bar_field_theme.dart';
-import 'package:get/get.dart';
-import 'package:lottie/lottie.dart';
+import 'package:iconsax/iconsax.dart';
+
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
-  final controller = Get.put(HomeController());
+
+  final TextEditingController txtSearch = TextEditingController();
+
+  final List catArr = [
+    {"image": "assets/img/cat_offer.png", "name": "Offers"},
+    {"image": "assets/img/cat_sri.png", "name": "Sri Lankan"},
+    {"image": "assets/img/cat_3.png", "name": "Italian"},
+    {"image": "assets/img/cat_4.png", "name": "Indian"},
+  ];
+
+  final List popArr = [
+    {
+      "image": "assets/img/res_1.png",
+      "name": "Minute by tuk tuk",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+    {
+      "image": "assets/img/res_2.png",
+      "name": "Café de Noir",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+    {
+      "image": "assets/img/res_3.png",
+      "name": "Bakes by Tella",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+  ];
+
+  final List mostPopArr = [
+    {
+      "image": "assets/img/m_res_1.png",
+      "name": "Minute by tuk tuk",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+    {
+      "image": "assets/img/m_res_2.png",
+      "name": "Café de Noir",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+  ];
+
+  final List recentArr = [
+    {
+      "image": "assets/img/item_1.png",
+      "name": "Mulberry Pizza by Josh",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+    {
+      "image": "assets/img/item_2.png",
+      "name": "Barita",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+    {
+      "image": "assets/img/item_3.png",
+      "name": "Pizza Rush Hour",
+      "rate": "4.9",
+      "rating": "124",
+      "type": "Cafa",
+      "food_type": "Western Food",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(() {
-        return CustomScrollView(
-          controller: controller.scrollController,
-          slivers: [
-            SliverAppBar(
-              pinned: false,
-              expandedHeight: 40,
-              flexibleSpace: Container(color: controller.appBarColor(context)),
-              title: Text(
-                FATexts.appName,
-                style: TextStyle(color: FAColors.white, fontSize: 32),
-              ),
-              centerTitle: true,
-            ),
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: ReactiveSearchHeader(),
-            ),
-            SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  LottieBuilder.asset("assets/images/home/homepage 1.json"),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 20,
-                    itemBuilder: (context, index) {
-                      return ListTile(title: Text('Item $index'));
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        );
-      }),
-    );
-  }
-}
-
-class ReactiveSearchHeader extends SliverPersistentHeaderDelegate {
-  final controller = Get.find<HomeController>();
-
-  @override
-  double get minExtent => 100;
-  @override
-  double get maxExtent => 100;
-
-  @override
-  Widget build(
-    BuildContext context,
-    double shrinkOffset,
-    bool overlapsContent,
-  ) {
-    return Obx(() {
-      final color = controller.appBarColor(context);
-
-      return Container(
-        height: FASizes.containerHeight,
-        width: double.infinity,
-        color: color,
+      body: SingleChildScrollView(
         child: Padding(
-          padding: FASpacingStyle.containerPadding,
-          child: TextFormField(
-            decoration: FASearchBarFieldTheme.getInputDecoration(
-              hintText: "Search...",
-              suffixIcon: Icon(Icons.search),
-            ),
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: [
+              const SizedBox(height: 46),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Good morning Kartikey",
+                      style: TextStyle(
+                        color: FAColors.primary,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => const MyOrderView(),
+                        //   ),
+                        // );
+                      },
+                      icon: Image.asset(
+                        "assets/img/shopping_cart.png",
+                        width: 25,
+                        height: 25,
+                        color: FAColors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Delivering to",
+                      style: TextStyle(
+                        color: FAColors.textSecondary,
+                        fontSize: 11,
+                      ),
+                    ),
+                    const SizedBox(height: 6),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Current Location",
+                          style: TextStyle(
+                            color: FAColors.textSecondary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(width: 25),
+                        Image.asset(
+                          "assets/img/dropdown.png",
+                          width: 12,
+                          height: 12,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: RoundTextfield(
+                  hintText: "Search Food",
+                  controller: txtSearch,
+                  left: Container(
+                    alignment: Alignment.center,
+                    width: 30,
+                    child: Image.asset(
+                      "assets/img/search.png",
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  itemCount: catArr.length,
+                  itemBuilder: ((context, index) {
+                    var cObj = catArr[index] as Map? ?? {};
+                    return CategoryCell(cObj: cObj, onTap: () {});
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ViewAllTitleRow(
+                  title: "Popular Restaurants",
+                  onView: () {},
+                ),
+              ),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                padding: EdgeInsets.zero,
+                itemCount: popArr.length,
+                itemBuilder: ((context, index) {
+                  var pObj = popArr[index] as Map? ?? {};
+                  return PopularRestaurantRow(pObj: pObj, onTap: () {});
+                }),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ViewAllTitleRow(title: "Most Popular", onView: () {}),
+              ),
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  itemCount: mostPopArr.length,
+                  itemBuilder: ((context, index) {
+                    var mObj = mostPopArr[index] as Map? ?? {};
+                    return MostPopularCell(mObj: mObj, onTap: () {});
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ViewAllTitleRow(title: "Recent Items", onView: () {}),
+              ),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                itemCount: recentArr.length,
+                itemBuilder: ((context, index) {
+                  var rObj = recentArr[index] as Map? ?? {};
+                  return RecentItemRow(rObj: rObj, onTap: () {});
+                }),
+              ),
+            ],
           ),
         ),
-      );
-    });
+      ),
+    );
   }
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
-      true;
 }
